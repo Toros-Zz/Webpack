001c1bd68e9b3494a126a4b4ec32aef8d16a7def
taras zherebetskiy <tarasj76@gmail.com>
2022-03-08 13:54:30 +0200

    webpack

---
+++ src/getLog.js
@@ -0,0 +1,8 @@
+import { wizard } from './wizard'
+import { barbarian } from './barbarian'
+
+export const log = () => {
+    console.log('sdds');
+    console.log(wizard);
+    console.log(barbarian);
+};
